7fb1fe01aee0235e4654dfa2f8b95d85f206061b
Harkamal Khabra <khabra07@gmail.com>
2019-02-19 13:36:06 +0000

    loopylighthouse exercise completed

--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,7 +1,11 @@
 for (var num = 100; num < 201; num ++) {
-  if (num % 3 === 0) {
+  if ( (num % 3 === 0) && (num % 4 === 0)) {
+    console.log("LoopyLighthouse")
+  } else if (num % 3 === 0) {
     console.log("Loopy")
+  } else if (num % 4 === 0) {
+    console.log("Lighthouse")
   } else {
-    console.log(num);
+    console.log(num)
   }
 }
